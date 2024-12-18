8a91b56c523db0a4df10896f05ef3ab9e09d7f99
AuntL1ke <semenkos2005@gmail.com.comgit config --global user.email semenkos2005@gmail.com>
2024-12-18 17:59:05 +0000

    navigation+delete+design

--- movies/urls.py
+++ movies/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-
 from movies import views
 
 urlpatterns = [
-    
-    path("list/", views.list),
-    path("details/<int:id>", views.details),
+    path("list/", views.list, name="list"),
+    path("details/<int:id>", views.details, name="details"),
+    path("delete/<int:id>", views.delete, name="delete"),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,17 +1,21 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, redirect, render
 from movies.models import Movie
 
-
-
 def list(request):
+ 
     movies = Movie.objects.all()
     return render(request, "index.html", {"movies": movies})
 
 
 def details(request, id):
-    movie = Movie.objects.get(id=id)
-    # TODO: check if user exists
 
+    movie = get_object_or_404(Movie, id=id)
     return render(request, "details.html", {"movie": movie})
+
+
+def delete(request, id):
+    if request.method == "POST":
+        movie = get_object_or_404(Movie, id=id)
+        movie.delete()
+        return redirect("list") 
+    return redirect("list")
